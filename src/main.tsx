8711ce459afe08fb98ff96945af8971e78a835a6
Choi Minjune <minjune0807@gmail.com>
2024-05-09 01:19:26 +0900

    feat: Globalstyle, theme 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import './index.css';
 
+import { Global, ThemeProvider } from '@emotion/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
+import reset from './styles/Reset.ts';
+import theme from './styles/Theme.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Global styles={reset} />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/styles/Theme.ts
@@ -0,0 +1,8 @@
+const theme = {
+  color: {
+    black: '#000',
+    white: '#fff',
+  },
+};
+
+export default theme;
