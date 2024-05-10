454cd9832b7d183d361d74e7465402be4ea555f6
taeseung-yoo <remicon99@gmail.com>
2024-05-11 01:50:27 +0900

    test: 권한 테스트!

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
 
   return (
     <>
+      <div>테스트!</div>
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
