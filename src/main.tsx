8fe60cdabd1e3dd72e1ee94db1e4fdf0da03856d
Yoo TaeSeung <59465914+taeseung-yoo@users.noreply.github.com>
2024-05-13 01:08:53 +0900

    Init: path alias 추가 (#10)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { Global, ThemeProvider } from '@emotion/react';
+import resetStyle from '@styles/reset.ts';
+import theme from '@styles/theme.ts';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
-import resetStyle from './styles/reset.ts';
-import theme from './styles/theme.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
