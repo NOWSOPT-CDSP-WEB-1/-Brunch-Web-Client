d8246474d314eb6e70fa772e73fbd616aa917cd8
jerry <becomegoodguy@gmail.com>
2024-05-14 00:07:16 +0900

    style: 공통 Color 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,21 @@
 const theme = {
   color: {
-    black: '#000',
-    white: '#fff',
+    white01: '#FFFFFF',
+    white02: 'rgba(255, 255, 255, 0.56)',
+    gray01: '#F8F8F8',
+    gray02: '#EEEEEE',
+    gray03: '#E6E6E6',
+    gray04: '#D5D5D3',
+    gray05: '#C9C9C9',
+    gray06: '#BEBEBE',
+    gray07: '#A8A8A8',
+    gray08: '#909090',
+    gray09: '#666666',
+    gray10: '#3D3D3D',
+    gray11: '#2D2D2D',
+    gray12: '#101010',
+    mint01: 'var(--mint01, #00C6BE)',
+    blue01: 'var(--blue01, #597ABE)',
   },
 };
 
