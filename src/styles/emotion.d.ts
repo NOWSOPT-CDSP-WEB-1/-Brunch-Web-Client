a309a5c80c330868efe65df97d5993005fa47cb2
jerry <becomegoodguy@gmail.com>
2024-05-14 01:18:08 +0900

    style: theme 타입 추가 (#12)

---
+++ src/styles/emotion.d.ts
@@ -0,0 +1,52 @@
+import type { SerializedStyles } from '@emotion/react';
+
+declare module '@emotion/react' {
+  export interface Theme {
+    color: {
+      white01: string;
+      white02: string;
+      gray01: string;
+      gray02: string;
+      gray03: string;
+      gray04: string;
+      gray05: string;
+      gray06: string;
+      gray07: string;
+      gray08: string;
+      gray09: string;
+      gray10: string;
+      gray11: string;
+      gray12: string;
+      mint01: string;
+      blue01: string;
+    };
+    font: {
+      display1: SerializedStyles;
+      head1: SerializedStyles;
+      head2: SerializedStyles;
+      head3: SerializedStyles;
+      head4: SerializedStyles;
+      head5: SerializedStyles;
+      head6: SerializedStyles;
+      body1: SerializedStyles;
+      body2: SerializedStyles;
+      body3: SerializedStyles;
+      body4_regular: SerializedStyles;
+      body4_light: SerializedStyles;
+      body5_3: SerializedStyles;
+      body5_10: SerializedStyles;
+      detail1: SerializedStyles;
+      detail2: SerializedStyles;
+      detail3_7: SerializedStyles;
+      detail3_12: SerializedStyles;
+      detail4_12: SerializedStyles;
+      detail4_183_0: SerializedStyles;
+      detail4_3: SerializedStyles;
+      detail5: SerializedStyles;
+      detail6: SerializedStyles;
+      caption1: SerializedStyles;
+      caption2: SerializedStyles;
+      caption3: SerializedStyles;
+    };
+  }
+}
