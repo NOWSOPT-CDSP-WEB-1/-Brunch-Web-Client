f0725fde22da6193352860a800251d87c94c8316
Choi Minjune <minjune0807@gmail.com>
2024-05-16 03:06:30 +0900

    feat: 배너UI구현(#11)

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,39 @@
-const home = () => {
-  return <div>home</div>;
+import styled from '@emotion/styled';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <Homecontainer>
+      <BannerWrapper>
+        <BannerImg src="banner.svg" alt="banner" />
+        <BannerCloseImg src="banner-close.svg" alt="banner-close" />
+      </BannerWrapper>
+    </Homecontainer>
+  );
 };
 
-export default home;
+export default Home;
+
+const Homecontainer = styled.div`
+  width: 100%;
+`;
+
+const BannerWrapper = styled.section`
+  position: relative;
+  display: flex;
+  width: 100%;
+`;
+
+const BannerImg = styled.img`
+  width: 100%;
+  height: 34.2rem;
+`;
+
+const BannerCloseImg = styled.img`
+  position: absolute;
+  top: 14.7rem;
+  left: 108.7rem;
+  display: flex;
+
+  cursor: pointer;
+`;
