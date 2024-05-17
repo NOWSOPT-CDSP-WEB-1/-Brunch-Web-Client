bfda44a5be620d93bc88b2fa693be6cc69ced897
jerry <becomegoodguy@gmail.com>
2024-05-17 14:02:30 +0900

    feat: 브런치 작가 신청 버튼 추가 (#25)

---
+++ src/pages/apply/_component/ApplyBtn.tsx
@@ -0,0 +1,40 @@
+import Arrow from '@assets/arrow.svg';
+import styled from '@emotion/styled';
+
+const ScrollFollowButton = () => {
+  return (
+    <BtnContainer>
+      <BtnText>
+        브런치 작가
+        <br /> 신청하기
+      </BtnText>
+      <Arrow />
+    </BtnContainer>
+  );
+};
+
+export default ScrollFollowButton;
+
+const BtnContainer = styled.button`
+  position: fixed;
+  top: 58rem;
+  right: 20rem;
+  display: flex;
+  flex-direction: column;
+  width: 14.2rem;
+  height: 14.2rem;
+  padding: 2.2rem 4.7rem 2rem 2rem;
+
+  text-align: start;
+
+  background-color: ${({ theme }) => theme.color.white01};
+  box-shadow: 0 4px 30.1px 7px rgb(0 0 0 / 16%);
+  cursor: pointer;
+`;
+
+const BtnText = styled.p`
+  margin-bottom: 1.5rem;
+
+  ${({ theme }) => theme.font.head4};
+  color: ${({ theme }) => theme.color.gray10};
+`;
