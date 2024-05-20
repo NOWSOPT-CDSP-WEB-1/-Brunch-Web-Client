1c27ea316a26f7e440601c1ae23b04b1f044c853
Jeongbowoon <happyanne200212@gmail/com>
2024-05-21 03:23:16 +0900

    design: right body UI 구현

--- src/components/libraryPage/recentBook.tsx
+++ src/components/libraryPage/recentBook.tsx
@@ -1,11 +1,16 @@
 import { Icon } from '@components';
 import styled from '@emotion/styled';
 import { icons } from '@styles/icons';
+import theme from '@styles/theme';
 import { useEffect, useState } from 'react';
+import { Book_size } from 'src/interface/Book_size';
 
+import BookImgForm from './BookImgForm';
 import FocusBook from './FocusBook';
 
 export default function RecentBook() {
+  const boxSize: Book_size = { x: '12rem', y: '15.7rem' };
+
   //여기서 최근 읽었던 책 목록 조회 -> recentBooks get
   const recentBooks = [
     {
@@ -22,6 +27,75 @@ export default function RecentBook() {
     },
     {
       id: 2,
+      title: '씁!! 가만히 좀 있어',
+      authorName: '노란콩',
+      bookImage: 'null',
+      description: '가만히 쉬는게 너무 어려운 사람이 살아가는 법.',
+      episode: 6,
+      requiredTime: 30,
+      progress: 50,
+      lastViewed: '2024-02-17T11:19:03',
+    },
+    {
+      id: 3,
+      title: '칼 든 괴한에게 쫓긴 밤',
+      authorName: '노마드',
+      bookImage: 'null',
+      description:
+        '새벽 4시, 밀라노를 걷다 칼 든 괴한에게 쫓겼습니다. 밀라노에 이르기까지의 여정, 그 밤, 그리고 하루하루를 살아가며 트라우마를 극복해가는 과정을 담았습니다. 죽을 위기를 넘기며 맞이한 가치관의 변화와 배운 교훈, 그리고 이후의 삶에 대해, 이제서야 써내릴 수 있어 글로 풀어냅니다.',
+      episode: 8, //focus 됐을 때 -화
+      requiredTime: 15, //focus 됐을 때
+      progress: 70,
+      lastViewed: '2024-01-17T11:19:06',
+    },
+    {
+      id: 4,
+      title: '씁!! 가만히 좀 있어',
+      authorName: '노란콩',
+      bookImage: 'null',
+      description: '가만히 쉬는게 너무 어려운 사람이 살아가는 법.',
+      episode: 6,
+      requiredTime: 30,
+      progress: 50,
+      lastViewed: '2024-02-17T11:19:03',
+    },
+    {
+      id: 5,
+      title: '칼 든 괴한에게 쫓긴 밤',
+      authorName: '노마드',
+      bookImage: 'null',
+      description:
+        '새벽 4시, 밀라노를 걷다 칼 든 괴한에게 쫓겼습니다. 밀라노에 이르기까지의 여정, 그 밤, 그리고 하루하루를 살아가며 트라우마를 극복해가는 과정을 담았습니다. 죽을 위기를 넘기며 맞이한 가치관의 변화와 배운 교훈, 그리고 이후의 삶에 대해, 이제서야 써내릴 수 있어 글로 풀어냅니다.',
+      episode: 8, //focus 됐을 때 -화
+      requiredTime: 15, //focus 됐을 때
+      progress: 70,
+      lastViewed: '2024-01-17T11:19:06',
+    },
+    {
+      id: 6,
+      title: '씁!! 가만히 좀 있어',
+      authorName: '노란콩',
+      bookImage: 'null',
+      description: '가만히 쉬는게 너무 어려운 사람이 살아가는 법.',
+      episode: 6,
+      requiredTime: 30,
+      progress: 50,
+      lastViewed: '2024-02-17T11:19:03',
+    },
+    {
+      id: 7,
+      title: '칼 든 괴한에게 쫓긴 밤',
+      authorName: '노마드',
+      bookImage: 'null',
+      description:
+        '새벽 4시, 밀라노를 걷다 칼 든 괴한에게 쫓겼습니다. 밀라노에 이르기까지의 여정, 그 밤, 그리고 하루하루를 살아가며 트라우마를 극복해가는 과정을 담았습니다. 죽을 위기를 넘기며 맞이한 가치관의 변화와 배운 교훈, 그리고 이후의 삶에 대해, 이제서야 써내릴 수 있어 글로 풀어냅니다.',
+      episode: 8, //focus 됐을 때 -화
+      requiredTime: 15, //focus 됐을 때
+      progress: 70,
+      lastViewed: '2024-01-17T11:19:06',
+    },
+    {
+      id: 8,
       title: '씁!! 가만히 좀 있어',
       authorName: '노란콩',
       bookImage: 'null',
@@ -57,6 +131,27 @@ export default function RecentBook() {
           lastViewed={recentBooks[focusIndex].lastViewed}
         />
       </LeftBody>
+      <RightBody>
+        <BookNavi>
+          <button onClick={() => {}}>
+            <Icon icon={icons.arrow_back_ios} color={theme.color.gray07} />
+          </button>
+          <NowNbr>{focusIndex + 1}</NowNbr>
+          {`/${recentBooks.length}`}
+          <button>
+            <Icon icon={icons.arrow_forward_ios} color={theme.color.gray07} />
+          </button>
+        </BookNavi>
+        <BookList>
+          {recentBooks.map((eachBook, index) => {
+            return (
+              <EachBookWrapper key={index + 1}>
+                <BookImgForm likedBook={eachBook} size={boxSize} />
+              </EachBookWrapper>
+            );
+          })}
+        </BookList>
+      </RightBody>
     </Container>
   );
 }
@@ -80,6 +175,33 @@ const TitleContainer = styled.div`
   display: flex;
 `;
 const Title = styled.p`
-  ${({ theme }) => theme.font.detail2}
-  color: ${({ theme }) => theme.color.gray11}
+  ${({ theme }) => theme.font.detail2};
+  color: ${({ theme }) => theme.color.gray11};
+`;
+const RightBody = styled.div`
+  width: 31rem;
+  padding: 1.2rem;
+  overflow: hidden;
+`;
+const BookNavi = styled.div`
+  display: flex;
+  align-items: center;
+  width: 12.2rem;
+  height: 4.8rem;
+
+  ${({ theme }) => theme.font.detail1};
+  color: ${({ theme }) => theme.color.gray07};
+`;
+const NowNbr = styled.p`
+  color: ${({ theme }) => theme.color.gray08};
+`;
+const BookList = styled.div`
+  display: inline-flex;
+  gap: 2.1rem;
+  overflow: visible;
+`;
+const EachBookWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.3rem;
 `;
--- src/styles/icons.ts
+++ src/styles/icons.ts
@@ -4,4 +4,5 @@ export const icons = {
   segment: { name: 'segment', className: 'material-symbols-outlined' },
   clock: { name: 'schedule', className: 'material-symbols-outlined' },
   arrow_forward_ios: { name: 'arrow_forward_ios', className: 'material-symbols-outlined' },
+  arrow_back_ios: { name: 'arrow_back_ios', className: 'material-symbols-outlined' },
 };
