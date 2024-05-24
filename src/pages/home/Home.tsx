19ccd4683a188638825163db530c7c12e8731873
gudusol <remicon99@gmail.com>
2024-05-24 17:04:44 +0900

    feat: 메인페이지 상단 배너 컴포넌트화 및 x 버튼 동작 추가

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,6 +3,7 @@ import styled from '@emotion/styled';
 
 import Apply from './_components/Apply';
 import Article from './_components/Aricle';
+import Banner from './_components/Banner';
 import DayArticle from './_components/DayArticle';
 import KeywordTable from './_components/KeywordTable';
 import MainCarousel from './_components/MainCarousel';
@@ -12,10 +13,7 @@ import MainWriter from './_components/MainWriter';
 const Home = () => {
   return (
     <Homecontainer>
-      <BannerWrapper>
-        <BannerImg src="banner.svg" alt="banner" />
-        <BannerCloseImg src="banner-close.svg" alt="banner-close" />
-      </BannerWrapper>
+      <Banner />
       <Header />
       <MainContainer>
         <MainIntro />
@@ -36,26 +34,6 @@ export default Home;
 
 const Homecontainer = styled.div`
   width: 100%;
-`;
-
-const BannerWrapper = styled.section`
-  position: relative;
-  display: flex;
-  width: 100%;
-`;
-
-const BannerImg = styled.img`
-  width: 100%;
-  height: 34.2rem;
-`;
-
-const BannerCloseImg = styled.img`
-  position: absolute;
-  top: 14.7rem;
-  left: 108.7rem;
-  display: flex;
-
-  cursor: pointer;
 `;
 
 const MainContainer = styled.main`
